c0d90ffa1e415e6f7389b9a89bf5f9ad4a82acdf
ambaker1 <ambaker1@mtu.edu>
2022-03-02 21:36:00 -0500

    Update PlotResponse.m

--- Convergence/PlotResponse.m
+++ Convergence/PlotResponse.m
@@ -17,11 +17,10 @@ title('Scale factor = 1.0')
 xlabel('Time (sec)')
 ylabel('X Disp (in)')
 hold on
-plot(time2,disp2)
-plot(time1,disp1)
-plot(time1(end),disp1(end),'sk')
-legend('Recursive Bisection','Original')
-
+plot(time2,disp2,'b-')
+plot(time1,disp1,'r--','LineWidth',2)
+plot(time1(end),disp1(end),'k*')
+legend('Recursive Bisection','Original','Failure Point')
 % Scale factor 5.0
 
 % Old convergence routine
@@ -38,10 +37,23 @@ title('Scale factor = 5.0')
 xlabel('Time (sec)')
 ylabel('X Disp (in)')
 hold on
-plot(time2,disp2)
-plot(time1,disp1)
-plot(time1(end),disp1(end),'sk')
-legend('Recursive Bisection','Original')
+plot(time2,disp2,'b-')
+plot(time1,disp1,'r--','LineWidth',2)
+plot(time1(end),disp1(end),'k*')
+legend('Recursive Bisection','Original','Failure Point')
+
+figure
+title('Detail @ Scale Factor = 5.0')
+xlabel('Time (sec)')
+ylabel('X Disp (in)')
+hold on
+plot(time2,disp2,'bo-')
+plot(time1,disp1,'ro--','LineWidth',2)
+plot(time1(end),disp1(end),'k*','MarkerSize',12)
+legend('Recursive Bisection','Original','Failure Point')
+xlim([3.85 3.91])
+ylim([4 7.5])
+
 
 
 % Scale factor 10.0
@@ -60,9 +72,23 @@ title('Scale factor = 10.0')
 xlabel('Time (sec)')
 ylabel('X Disp (in)')
 hold on
-plot(time2,disp2)
-plot(time1,disp1)
-plot(time1(end),disp1(end),'sk')
-legend('Recursive Bisection','Original')
+plot(time2,disp2,'b-')
+plot(time1,disp1,'r--','LineWidth',2)
+plot(time1(end),disp1(end),'k*')
+plot(time2(end),disp2(end),'k*')
+legend('Recursive Bisection','Original','Failure Points')
+
+
+figure
+title('Detail @ Collapse (Scale Factor = 10.0)')
+xlabel('Time (sec)')
+ylabel('X Disp (in)')
+hold on
+plot(time2,disp2,'bo-')
+plot(time2(end),disp2(end),'k*','MarkerSize',12)
+legend('Recursive Bisection','Failure Point')
+
+xlim([8.205 8.245])
+ylim([-21 -12])
 
 
